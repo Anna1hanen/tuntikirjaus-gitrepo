5596109d3f50652687d4d7942ae7edb4be0765c7
Elina <elina.ylkanen.gcp@gmail.com>
2021-11-10 15:39:45 +0200

    päivitystä

--- src/python/scripts/tuntikirjaus.py
+++ src/python/scripts/tuntikirjaus.py
@@ -1,72 +1,36 @@
-import datetime
-from data import config as config
 import psycopg2
-
-menu_commands = "1: Lisää uusi tuntikirja \n" \
-                "2: Katso kuluneen viikon tuntikirjaukset \n" \
-                "3: Poistu\n>"
+from data import config as config
+import datetime
 
 class Tuntikirja:
     def __init__(self, start_date = 0, end_date = 0, start_time = 0, end_time = 0, project_name = "", definition = ""):
-
+        # self.date = aloituspäivä talteen loppupäivän vertailua varten
         self.start_date = start_date
         self.end_date = end_date
         self.start_time = start_time
         self.end_time = end_time
         self.project_name = project_name
         self.definition = definition
-
+        
+    def menu():
+        return "Hello world!"
 
     def set_start_date(self):
-        while True:
-            try:
-                date_entry = input("Anna aloituspäivämäärä muodossa DD/MM/YYYY\n>")
-                day, month, year = map(int, date_entry.split('/'))
-                self.start_date = datetime.date(year, month, day)
-                return self.start_date
-            except Exception as e:
-                print(f"Virheellinen syöte, {e}")
-                continue
+        date_entry = input("Anna aloituspäivämäärä muodossa DD/MM/YYYY")
+        day, month, year = map(int, date_entry.split('/'))
+        self.start_date = datetime.date(year, month, day)
+        print(self.start_date)
 
+    def set_start_time():
+        pass
 
-    def set_start_time(self):
-        while True:
-            try:
-                date_entry = input("Anna aloitusaika muodossa HH:MM\n>")
-                hour, minute = map(int, date_entry.split(':'))
-                self.start_time = datetime.time(hour, minute)
-                return self.start_time
-            except Exception as e:
-                print(f"Virheellinen syöte, {e}")
+    def set_end_date():
+        pass
 
+    def set_end_time():
+        pass
 
-    def set_end_date(self):
-        enddate_entry = input("Anna aloituspäivämäärä muodossa DD/MM/YYYY")
-        day, month, year = map(int, enddate_entry.split('/'))
-        self.end_date = datetime.date(year, month, day)
-        if self.start_date > self.end_date:
-            valinta = input(f"Lopetuspäivämäärä ({self.end_date}) on ennen aloituspäivämäärää ({self.start_date}), haluaisitko muuttaa sen? Valitse k tai e")
-            if valinta == 'k' or valinta =='K':
-                return self.start_date()
-            else:
-                return self.end_date
-
-    def set_end_time(self):
-        endtime_entry = input("Anna lopetusaika muodossa HH:MM")
-        self.end_time = datetime.datetime.strptime(endtime_entry, '%H:%M').time()
-        end_time_and_end_date = date.self.end_date + time.self.end_time
-        start_time_and_start_date = date.self.start_date + time.self.end_time
-        if start_time_and_start_date > end_time_and_end_date:
-            valinta = input(f"Työn lopetusajankohta ({self.end_time}) ei voi ennen aloitusajankohtaa ({self.start_time}). Haluatko muuttaa aloitusajankohtaa? Vastaa k, jos haluat")
-            if valinta == 'k' or valinta == 'K':
-                return set_start_date()
-        
-            return self.end_time
-
-
-
-        
-     def set_project_name(self):
+    def set_project_name(self):
         self.project_name = input("Anna projektin nimi")
         return self.project_name
         
@@ -91,63 +55,12 @@ class Tuntikirja:
     #         con.close()
 
 
-        pass
-
-    def set_definition(self):
 
         pass
 
-    def __str__(self):
-        return f"Aloitus päivä: {self.start_date}\n" \
-               f"Aloitus aika: {self.start_time}\n" \
-               f"Lopetus päivä: {self.end_date}\n" \
-               f"Lopetus aika: {self.end_time}\n" \
-               f"Projektin nimi: {self.project_name}\n" \
-               f"Selite: {self.definition}"
-
-
-def menu():
-    while True:
-        try:
-            command = int(input(menu_commands))
-            if command == 1:
-                # tee uusi tuntikirja
-                make_new_worklog()
-            elif command == 2:
-                # katso tuntikirjaukset
-                pass
-            elif command == 3:
-                # lopettaa ohjelman
-                break
-            else:
-                print("Virheellinen syöte")
-        except Exception as e:
-            # tuli virhe jossain päin koodia
-
-            # raise tarkempaa testausta varten
-            # raise e
-
-            # raaka errorin printti
-            print(f"Virheellinen syöte, {e}")
-
-            # käyttäjäystävällisempi printti?
-            # print("Voihan juukeli jokin meni pieleen")
-
-
-def make_new_worklog():
-    tuntikirja = Tuntikirja()
-    tuntikirja.set_start_date()
-    tuntikirja.set_start_time()
-    print(tuntikirja)
-
-    insert_to_database()
-
-
-def insert_to_database():
-    # Laitetaan data databaseen
-    pass
-
-
+    def set_definition():
+        pass
+    
 if __name__ == "__main__":
-    menu()
-
+    #tunnit = Tuntikirja()
+    print(Tuntikirja.project())
